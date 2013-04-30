ab4e918856649fef30e0228647ec73b1457f3a34
Chyld Medford <chyld.medford@gmail.com>
2013-04-30 16:04:35 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prometheus::Application.config.secret_token = '06bbe49d3b0c40e9fafdcea5cb982937fe52053156ffe53ab881983b470af9d61924acd21edc062132b76650706d4674370f55b6b9d7282fca23dbe49e2a561c'
